1119d7f8bbb94245a75508e0e6665b8fd321be49
vutuananh <anhvubku@gmail.com>
2018-04-11 23:28:28 +0700

    Initial code

---
+++ Best Market/app/src/main/java/com/caocao/bestmarket/MainActivity.java
@@ -0,0 +1,47 @@
+package com.caocao.bestmarket;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+
+                            case R.id.action_hơme:
+                                selectedFragment = new HomeFragment();
+                                break;
+
+                            case R.id.action_cart:
+                                selectedFragment= new CartFragment();
+                                break;
+
+                        }
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                selectedFragment).commit();
+                        return true;
+                    }
+                });
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }
+    }
+}
